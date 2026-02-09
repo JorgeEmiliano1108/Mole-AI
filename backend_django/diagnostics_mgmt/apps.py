d61766a881b81e8cbe32cf210e2780c67ec4dded
JorgeEmiliano1108 <jorgee.gf27816@gmail.com>
2026-02-09 09:06:03 -0600

    Delete backend_django directory

--- backend_django/diagnostics_mgmt/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DiagnosticsMgmtConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'diagnostics_mgmt'
--- backend_django/plants_mgmt/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlantsMgmtConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'plants_mgmt'
